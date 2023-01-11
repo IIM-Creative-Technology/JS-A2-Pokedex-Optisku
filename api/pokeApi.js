e82285fdddd86f1b63e8558f5561e9ea8d3cbb91
deez-blip <gaspard.castagnet92@gmail.com>
2023-01-11 16:30:42 +0100

    affichage pokemons

--- api/pokeApi.js
+++ api/pokeApi.js
@@ -2,13 +2,16 @@ let pokemons = []
 let url = "https://pokeapi.co/api/v2/pokemon"
 let pkm_nbr = 151
 
+let container = document.querySelector('.container')
+
 const fetchPokemons = async () => {
     for(let i = 1; i <= pkm_nbr; i++) {
         await getAllPokemon(i)
     }
-    pokemons.forEach((pokemon) =>
-        console.log(pokemon)
-    )
+    pokemons.forEach(pokemon => {
+        showPokemon(pokemon)
+    })
+    console.log(pokemons[3])
 }
 
 const getAllPokemon = async (id) => {
@@ -18,9 +21,30 @@ const getAllPokemon = async (id) => {
     pokemons.push(pokemonAdd)
 }
 
-const showPokemon = async (link) => {
-    for(let i = 1; i <= pkm_nbr; i++) {
-        await getAllPokemon(i)
+const showPokemon = async (pokemon) => {
+    if(typeof pokemon.types[1] != "undefined"){ 
+        container.innerHTML += 
+            `
+            <div class="pokemon">
+                <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+                <h2>${pokemon.name}</h2>
+                <div class="types">
+                    <h3>${pokemon.types[0].type.name}</h3>
+                    <h3>${pokemon.types[1].type.name}</h3>
+                </div>
+            </div>
+            `
+    }else {
+        container.innerHTML +=
+            `
+            <div class="pokemon">
+                <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+                <h2>${pokemon.name}</h2>
+                <div class="types">
+                    <h3>${pokemon.types[0].type.name}</h3>
+                </div>
+            </div>
+            `
     }
 }
 fetchPokemons()
