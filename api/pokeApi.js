1daa73af045d5ae072905af47ad89c342b904c7b
Angel Hmeli <angel.hmeli@edu.devinci.fr>
2023-01-13 02:22:05 +0100

    Secret code

--- api/pokeApi.js
+++ api/pokeApi.js
@@ -185,3 +185,16 @@ function wait(ms) {
 //     }
 //     wait(1000)
 // };
+
+
+const pressed = []
+const secretCode = 'battle!'
+window.addEventListener('keyup', (e) => {
+  console.log(e.key)
+  pressed.push(e.key)
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length)
+  if (pressed.join('').includes(secretCode)) {
+    window.location.href ="https://play.pokemonshowdown.com/"
+  }
+})
+
